4302211299a73639622e3d5b09bd13df1e2e8d52
Gabriel Godoy <gabrielgodoyon@gmail.com>
2022-08-31 16:20:19 -0300

    fix: marketing npm install on workflow

--- packages/marketing/src/components/Landing.js
+++ packages/marketing/src/components/Landing.js
@@ -54,7 +54,7 @@ const useStyles = makeStyles((theme) => {
       flexDirection: 'column',
     },
     cardMedia: {
-      paddingTop: '56.25%', // 16:9
+      paddingTop: '56.25%',
     },
     cardContent: {
       flexGrow: 1,
